62011e626bb825eb381bed37a13b45a8593ccf76
marb <marvellchristiansutanto@gmail.com>
2026-05-27 16:11:13 +0800

    singleton init hehehe

--- src/backend_new/translation_analysis.py
+++ src/backend_new/translation_analysis.py
@@ -1,8 +1,21 @@
-class Translator:
+class LLMModel:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._initialized = False
+
+        return cls._instance
+
     def __init__(self) -> None:
+        if getattr(self, "_initialized", False):
+            return
+
         # TODO ask use to choose what model
         self._llm = None
         self._pipe = None
+
         # model save loc
         import os
         from pathlib import Path
@@ -11,6 +24,8 @@ class Translator:
         self._model_dir = Path(self._base_dir / "models/hugging_face")
         os.environ["HF_HOME"] = str(self._model_dir)
         self._model_id = str(self._model_dir / "shisa-v2.1-qwen3-8b-awq")
+
+        self._initialized = True
 
     # region batched inference
     # TODO trying out batched, maybe add a feature where it monitors GPU usage and changes batch size
@@ -25,8 +40,12 @@ class Translator:
 
         return [response.text for response in self._pipe(prompts)]
 
-if __name__ == "__main__":
-    llm_model = Translator()
-    data = llm_model.batch_inference(["what is your name?", "what languages do you support?"])
 
-    print(data)
+
+if __name__ == "__main__":
+    llm_model = LLMModel()
+    llm_model2 = LLMModel()
+
+    # data = llm_model.batch_inference(["what is your name?", "what languages do you support?"])
+    #
+    # print(data)
