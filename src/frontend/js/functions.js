0451ce53e34cce103b30a84aa4301b3a0e123b5a
Kaito Hirashima <alto.hsr12345@gmail.com>
2026-08-05 23:13:20 +0800

    Made a second page and a entry transition into the second page. Also added a temporary return function

--- src/frontend/js/functions.js
+++ src/frontend/js/functions.js
@@ -1,5 +1,7 @@
 // Below declares all used variables
 let StartUp = false;
+
+
 const circlec = document.getElementById("circlecontainer");
 const appbackground=document.getElementById("app");
 const circleout=document.getElementById("circleoutline");
@@ -9,8 +11,18 @@ const titletextcontainer = document.getElementById("titlecontainer");
 
 // Below are all the functions that will be used
 
+function delay(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+} // function to create delay in milliseconds
+
+
+// this is a temporary way to go back to the first page as i havent added a proper way to return. Terrible functionality btw just reload the page when u come
+function returnback() {
+        window.location.href="page1.html";
+}
+
 // this function starts the transition for the first to second page
-function StartMenuTransition() {
+async function StartMenuTransition() {
     if (StartUp === true) return;
     console.log("StartMenu");
     StartUp = true;
@@ -20,4 +32,6 @@ function StartMenuTransition() {
     circleout.style.border = "#887aab";
     clickanywhere.style.color="#887aab";
     titletextcontainer.style.opacity = 0;
+    await delay(400);
+    window.location.href= "page2.html";
 }
--- src/frontend/js/script.js
+++ src/frontend/js/script.js
@@ -1,1 +1,6 @@
-document.addEventListener("click", StartMenuTransition)
+document.addEventListener("click", StartMenuTransition);
+document.addEventListener( "keydown", (keypress) => {
+    if (keypress.key === "Escape") {
+        returnback();
+    }
+}) // this is a temporary way to go back to the first page as i havent added a proper way to return
