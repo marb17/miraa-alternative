12dd1fb128aa799637031f3a169deefff1696b5d
marb <marvellchristiansutanto@gmail.com>
2026-09-07 00:13:52 +0800

    back to what i did last time i guess

--- src/engine/core/processing.py
+++ src/engine/core/processing.py
@@ -14,8 +14,6 @@ import sys
 import multiprocessing
 import subprocess
 
-import whisper
-
 from engine.utils.classes.dataclasses import UIPromptRequest
 # HELPER LIBRARIES
 from engine.utils.functions.filesystem import read_json_file
@@ -30,9 +28,6 @@ from engine.utils.default.default_var import AUDIO_MODEL_PRESETS
 # import nagisa
 
 from pydub import AudioSegment
-import whisperx
-import stable_whisper
-from stable_whisper.audio import load_audio
 
 from engine.utils.logger import Logger
 logger = Logger(__name__)
@@ -213,22 +208,7 @@ class ForcedAlignment:
         ...
 
     def force_align_lyrics(self, audio_file: Path, json_data_file: Path) -> Any:
-        env = os.environ.copy()
-        env["PYTHONUTF8"] = "1"
-        env["HF_HOME"] = str(MODEL_DIR / "whisperx")
-
-        # model = whisperx.load_model("kotoba-tech/kotoba-whisper-bilingual-v1.0-faster",
-        #                                   download_root=str(MODEL_DIR / "whisperx"),
-        #                                   device="cuda")
-        model = stable_whisper.load_hf_whisper("kotoba-tech/kotoba-whisper-bilingual-v1.0",
-                                          download_root=str(MODEL_DIR / "whisperx"),
-                                          # device="cpu",
-                                          device="cuda",
-                                          # compute_type="bfloat16",
-                                          # cpu_threads=4,
-                                          )
-
-        audio = load_audio(str(audio_file))
+        os.environ["PYTHONUTF8"] = "1"
 
         lyrics = read_json_file(json_data_file).get("lyrics_main", "").split("\n")
 
@@ -243,33 +223,19 @@ class ForcedAlignment:
                 mapping_index.append((lyric, "do"))
 
         process_lyrics = [lyric[0] for lyric in mapping_index if lyric[1] == "do"]
-        process_lyrics = "".join(process_lyrics)
-        # process_lyrics = "\n".join(process_lyrics)
+        # process_lyrics = "".join(process_lyrics)
+        process_lyrics = "\n".join(process_lyrics)
 
         # for lyric in process_lyrics: print(lyric)
         print(process_lyrics)
 
+        temp_lyric_path = Path(json_data_file.parent / f"{json_data_file.stem}_lyrics.txt)")
+        temp_lyric_path.write_text(process_lyrics,
+                                   encoding="utf-8")
 
-        result = model.align(audio, process_lyrics,
-                             language="ja",
-                             vad=True,
-                             vad_threshold=0.5,
-                             original_split=True,
-                             min_word_dur=0.08,
-                             # no_speech_threshold=1,
-                             # failure_threshold=0.3,
-                             # fast_mode=True,
-                             )
-
-        print(result)
-
-        # result = model.refine(
-        #     str(audio_file),
-        #     result,
-        #     precision=0.05,
-        # )
-
-        # result.save_as_json(str(Path(json_data_file.parent / f"{json_data_file.stem}_lyrics")))
+        subprocess.run(
+            ["lyric-align", str(audio_file), str(temp_lyric_path), "-o", str(temp_lyric_path)]
+        )
 
 
 if __name__ == "__main__":
--- src/helper_functions/.temp/temp.py
+++ src/helper_functions/.temp/temp.py
@@ -1,38 +1,38 @@
-# from pathlib import Path
-# import json
-# from engine.utils.functions.filesystem import read_json_file
-# import playsound3
-# import time
-#
-# file_path = Path(r"D:\python\miraa-alternative\src\.temp\想い人 - Ryokuoushoku Shakai_lyrics.json")
-#
-# data = read_json_file(file_path)
-# file_path.write_text(json.dumps(data, indent=4))
-#
-# segements = data.get("segments")
-# print(segements)
-#
-# playsound3.playsound(Path(r"D:\python\miraa-alternative\src\.temp\QLBfxG0cenQ.wav"), block=False)
-# start_time = time.time()
-#
-# last_seg_start = 0
-#
-# while True:
-#     if time.time() - start_time > segements[0].get("end"):
-#         segements.pop(0)
-#
-#     if time.time() - start_time > segements[0].get("start") and last_seg_start != segements[0].get("start"):
-#         last_seg_start = segements[0].get("start")
-#         print(segements[0]["text"])
-#
-#     # print(time.time() - start_time)
-#     time.sleep(0.05)
+from pathlib import Path
+import json
+from engine.utils.functions.filesystem import read_json_file
+import playsound3
+import time
 
-import torch
+file_path = Path(r"D:\python\miraa-alternative\src\.temp\test.json")
 
-print(f"CUDA Available: {torch.cuda.is_available()}")
+data = read_json_file(file_path)
+file_path.write_text(json.dumps(data, indent=4))
 
-print(f"GPU Count: {torch.cuda.device_count()}")
+segements = data.get("segments")
+print(segements)
 
-if torch.cuda.is_available():
-    print(f"Current Device Name: {torch.cuda.get_device_name(0)}")
+playsound3.playsound(Path(r"D:\python\miraa-alternative\src\.temp\aRDURmIYBZ4.wav"), block=False)
+start_time = time.time()
+
+last_seg_start = 0
+
+while True:
+    if time.time() - start_time > segements[0].get("end"):
+        segements.pop(0)
+
+    if time.time() - start_time > segements[0].get("start") and last_seg_start != segements[0].get("start"):
+        last_seg_start = segements[0].get("start")
+        print(segements[0]["text"])
+
+    # print(time.time() - start_time)
+    time.sleep(0.05)
+
+# import torch
+#
+# print(f"CUDA Available: {torch.cuda.is_available()}")
+#
+# print(f"GPU Count: {torch.cuda.device_count()}")
+#
+# if torch.cuda.is_available():
+#     print(f"Current Device Name: {torch.cuda.get_device_name(0)}")
